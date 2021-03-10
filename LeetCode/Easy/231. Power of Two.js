01815a3a190de9653790a598d03ac3c49320ce0f
Sergei Shneider <50211129+SShneider@users.noreply.github.com>
2021-03-10 13:05:56 -0500

    Create 231. Power of Two.js

---
+++ LeetCode/Easy/231. Power of Two.js
@@ -0,0 +1,9 @@
+var isPowerOfTwo = function(n) {
+    if(n===1) return true
+    if(n%2!==0 || n <= 0) return false
+    while(n>1){
+        if(n%2!==0) return false
+        n/=2
+    }
+    return true
+};
